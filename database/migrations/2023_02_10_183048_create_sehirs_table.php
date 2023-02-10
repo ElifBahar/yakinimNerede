3584d502747be356ba3321802579d971a0df8b5c
ucercagatay <cagataygia@gmail.com>
2023-02-10 21:31:52 +0300

    Migration and models added columns need

---
+++ database/migrations/2023_02_10_183048_create_sehirs_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSehirsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sehirs', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('sehirs');
+    }
+}
