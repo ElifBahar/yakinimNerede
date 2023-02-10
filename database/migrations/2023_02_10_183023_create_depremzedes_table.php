9e8799678d18433dbc2d3113b0e5e53ffb1a7057
ucercagatay <cagataygia@gmail.com>
2023-02-10 21:37:04 +0300

    depremzede table done

--- database/migrations/2023_02_10_183023_create_depremzedes_table.php
+++ database/migrations/2023_02_10_183023_create_depremzedes_table.php
@@ -15,6 +15,17 @@ class CreateDepremzedesTable extends Migration
     {
         Schema::create('depremzedes', function (Blueprint $table) {
             $table->id();
+            $table->string('name')->nullable();
+            $table->string('surname')->nullable();
+            $table->string('tc_no')->nullable();
+            $table->string('yas')->nullable();
+            $table->string('mother_name')->nullable();
+            $table->string('father_name')->nullable();
+            $table->boolean('type');
+            $table->boolean('gender')->nullable();
+            $table->boolean('is_adult');
+            $table->unsignedBigInteger('city_id');
+            $table->dateTime('cikis_saati')->nullable();
             $table->timestamps();
         });
     }
